f08c6c2467211966d554777df10c82a63e659101
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-10-12 15:20:56 +0530

    install react-router-dom and setup the routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navigator from "./navigator/Navigator";
 
 const App: React.FC = () => {
 
   return (
     <>
-
+      <Router>
+        <Navigator />
+      </Router>
     </>
   )
 }
---
+++ client/src/navigator/Navigator.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+const Navigator: React.FC = () => {
+
+    return (
+        <>
+            <Routes>
+
+            </Routes>
+        </>
+    )
+}
+
+export default Navigator;
