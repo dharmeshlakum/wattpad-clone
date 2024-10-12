8f2100b0c206d6c4d604e9b0a741e07c747f73d7
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-10-12 14:49:05 +0530

    basi setup for the app.ts

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import { join } from "path";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// path setups
+const profilePicturesPath = join(__dirname, "./assets/profilePictures");
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "*",
+    methods: "*"
+}));
+app.use("/user/profile-pic", express.static(profilePicturesPath));
+
+// routes
+
+// server listen
+app.listen(port, ()=>{
+    console.log("Server is running on port :", port);
+});
