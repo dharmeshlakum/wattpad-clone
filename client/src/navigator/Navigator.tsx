08355f48e15d140a969a4b4d3d5ffe2dd28068c3
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-10-12 18:02:09 +0530

    added navbar in the navigattion router

--- client/src/navigator/Navigator.tsx
+++ client/src/navigator/Navigator.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "../components/navbar/Navbar";
 
 const Navigator: React.FC = () => {
 
     return (
         <>
+            <Navbar />
             <Routes>
-
+                
             </Routes>
         </>
     )
