52c3a604d0fb6c50be9f2b367bcfeb107a2cefb4
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-11-08 09:25:20 +0530

    created login collection schema

---
+++ server/src/models/logins/loginCollection.ts
@@ -0,0 +1,45 @@
+import mongoose, { Document, Schema, model } from "mongoose";
+
+// interface to define the schema type
+interface ILogin {
+    userId: mongoose.Schema.Types.ObjectId,
+    userAgent: string;
+    ipAddress: string;
+    token: string;
+    loginAt: Date
+}
+
+const loginCollectionSchema = new Schema<ILogin>({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Users",
+        required: true
+    },
+
+    userAgent: {
+        type: String,
+        required: true
+    },
+
+    ipAddress: {
+        type: String,
+        required: true
+    },
+
+    token: {
+        type: String,
+        required: true
+    },
+
+    loginAt: {
+        type: Date,
+        default: Date.now,
+        index: {
+            expireAfterSeconds: 60 * 60 * 24 * 7
+        }
+    }
+});
+
+const loginModel = model("Logins", loginCollectionSchema);
+export default loginModel;
