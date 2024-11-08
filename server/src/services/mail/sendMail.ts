b3c8a40a274d34fe25966a2139b3f81527f278c2
dharmeshlakum <lakumdharmesh60@gmail.com>
2024-11-08 10:54:31 +0530

    created services to send the mail

---
+++ server/src/services/mail/sendMail.ts
@@ -0,0 +1,38 @@
+import { createTransport } from "nodemailer";
+
+const transporter = createTransport({
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD
+    }
+});
+
+// function to send the otp on the email address
+const sendMailFN = (emailAddress: string, subject: string, text: string): void => {
+
+    try {
+        const mailOption = {
+            from: process.env.MAIL_USER,
+            to: emailAddress,
+            subject,
+            text
+        }
+        transporter.sendMail(mailOption, (err, info) => {
+
+            if (err) {
+                console.log("mail sending function error :", err);
+                return;
+
+            } else {
+                console.log("Mail sended successfully :", info.response);
+                return;
+            }
+        })
+
+    } catch (error: any) {
+        console.log("Sending mail function error :", error);
+        return;
+    }
+}
+
+export default sendMailFN
